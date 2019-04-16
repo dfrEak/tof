676f0b19b1b8b173382ea404e1c7df00ca6edd27
dfrEak <eric.c@gmail.com>
2019-04-16 13:10:35 +0900

    video streaming ok

--- server/video/video_client.py
+++ server/video/video_client.py
@@ -10,19 +10,31 @@ conn = client_sock.makefile('wb')
 
 try:
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,320)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,240)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
 
-    start = time.time()
+    #start = time.time()
 
+    print("start")
     while(cap.isOpened()):
         conn.flush()
         ret, frame = cap.read()
+        
+        
         byteImage = frame.tobytes()
         conn.write(byteImage)
+        print(len(byteImage))
 
+        # Display the resulting frame
+        if ret == True:
+            cv2.imshow('frame',frame)
+        else:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 finally:
+    print("finally")
     finish = time.time()
     cap.release()
     client_sock.close()
--- server/video/video_server.py
+++ server/video/video_server.py
@@ -1,4 +1,4 @@
-import SocketServer
+import socketserver
 import threading
 import numpy as np
 import cv2
@@ -8,7 +8,7 @@ import sys
 ultrasonic_data = None
 
 #BaseRequestHandler is used to process incoming requests
-class UltrasonicHandler(SocketServer.BaseRequestHandler):
+class UltrasonicHandler(socketserver.BaseRequestHandler):
 
     data = " "
 
@@ -21,38 +21,29 @@ class UltrasonicHandler(SocketServer.BaseRequestHandler):
 
 
 #VideoStreamHandler uses streams which are file-like objects for communication
-class VideoStreamHandler(SocketServer.StreamRequestHandler):
-"""
+class VideoStreamHandler(socketserver.StreamRequestHandler):
+    
+    
     def handle(self):
         stream_bytes = b''
+        IMAGE_SIZE = 320*240*3
+        print(len(stream_bytes))
 
-        try:
-            stream_bytes += self.rfile.read(1024)
-            image = np.frombuffer(stream_bytes, dtype="B")
-            print(image.shape)
-            cv2.imshow('F', image)
-            cv2.waitKey(0)
-
-        finally:
-            cv2.destroyAllWindows()
-            sys.exit()
-"""         
-    IMAGE_SIZE = 320*240*3
-
-    def handle(self):
-        stream_bytes = b''
-
-        try:
-            stream_bytes += self.rfile.read(1024)
-            while len(stream_bytes) >= IMAGE_SIZE:
-                image = np.frombuffer(stream_bytes[:IMAGE_SIZE], dtype="B")
-                stream_bytes = stream_bytes[IMAGE_SIZE:]
-                print(image.shape)
-                cv2.imshow('F', image)
-                cv2.waitKey(0)
-        finally:
-            cv2.destroyAllWindows()
-            sys.exit()
+        while(True):
+            try:
+                stream_bytes += self.rfile.read(1024)
+                print(len(stream_bytes))
+                while len(stream_bytes) >= IMAGE_SIZE:
+                    print("get data")
+                    image = np.frombuffer(stream_bytes[:IMAGE_SIZE], dtype="B").reshape(240, 320,3)
+                    stream_bytes = stream_bytes[IMAGE_SIZE:]
+                    print(image.shape)
+                    cv2.imshow('F', image)
+                    cv2.waitKey(1)
+            finally:
+                print("finally")
+                #cv2.destroyAllWindows()
+                #sys.exit()
 
 class Self_Driver_Server:
 
@@ -63,12 +54,12 @@ class Self_Driver_Server:
 
     def startUltrasonicServer(self):
         # Create the Ultrasonic server, binding to localhost on port 50001
-        server = SocketServer.TCPServer((self.host, self.portUS), UltrasonicHandler)
+        server = socketserver.TCPServer((self.host, self.portUS), UltrasonicHandler)
         server.serve_forever()
 
     def startVideoServer(self):
         # Create the video server, binding to localhost on port 50002
-        server = SocketServer.TCPServer((self.host, self.portCam), VideoStreamHandler)
+        server = socketserver.TCPServer((self.host, self.portCam), VideoStreamHandler)
         server.serve_forever()
 
     def start(self):
@@ -78,10 +69,11 @@ class Self_Driver_Server:
         self.startVideoServer()
 
 
+
 if __name__ == "__main__":
 
-    #From SocketServer documentation
-    HOST, PORTUS, PORTCAM = '192.168.0.18', 50001, 50002
+    #From socketserver documentation
+    HOST, PORTUS, PORTCAM = '192.168.1.178', 50001, 50002
     sdc = Self_Driver_Server(HOST, PORTUS, PORTCAM)
 
     sdc.start()
